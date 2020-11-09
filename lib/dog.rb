c44cc65e846c2b592c03a7736ae0a266df9ca412
krae <kraewind@gmail.com>
2020-11-09 14:07:00 -0500

    commit tree 38645d75ca0553add8b78aa5b160042ca6abe4e8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  @@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    #if !(@@all.any? {|dog| dog.name == @name})
+    @@all << self
+  
+  end
+
+  def self.all
+    @@all
+  end
 end
